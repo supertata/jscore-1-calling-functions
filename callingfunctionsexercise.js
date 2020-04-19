7e0e4645793b5df2df0e3893c28178e0d57ab93f
Laetitia Kinja <Laetitiawork@Laetitias-MacBook-Pro.local>
2020-04-19 17:39:13 +0200

    done step 1 and 2

--- callingfunctionsexercise.js
+++ callingfunctionsexercise.js
@@ -3,8 +3,21 @@ function calculateBirthYear (a) {
     const currentYear = 2020;
 let age = a;
 
-return (currentYear - a);
+return (currentYear - age);
 
 }
 
-console.log(calculateBirthYear(30));
+console.log(calculateBirthYear(3));
+
+
+function nameAndAge (n,a) {
+   let age = a;
+   let name = n;
+   const birthYear = calculateBirthYear(a);
+   let message = "The person's" + " " + name + " " + "and they were born in" + " " + birthYear;
+
+
+   return message;
+}
+
+console.log(nameAndAge("Laetitia", 30));
