8d019dfaef23468d18ce1c81c82210416ee96cc6
Laetitia Kinja <Laetitiawork@Laetitias-MacBook-Pro.local>
2020-04-20 22:30:40 +0200

    removed unecessary variables

--- callingfunctionsexercise.js
+++ callingfunctionsexercise.js
@@ -1,23 +1,14 @@
-function calculateBirthYear (a) {
-
-    const currentYear = 2020;
-let age = a;
-
-return (currentYear - age);
-
+function calculateBirthYear (age) {
+return (2020 - age);
 }
 
 console.log(calculateBirthYear(3));
 
 
-function nameAndAge (n,a) {
-   let age = a;
-   let name = n;
-   const birthYear = calculateBirthYear(a);
-   let message = "The person's" + " " + name + " " + "and they were born in" + " " + birthYear;
-
-
+function nameAndAge (name, age) {
+   const birthYear = calculateBirthYear(age);
+   const message = "The person's" + " " + name + " " + "and they were born in" + " " + birthYear;
    return message;
 }
 
-console.log(nameAndAge("Laetitia", 30));
+console.log(nameAndAge("John", 12));
